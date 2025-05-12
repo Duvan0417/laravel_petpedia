a6ee419cc7531232807a2379b354b2d8e324e070
Anderson David <andersonramosg1995@gmail.com>
2025-05-12 16:46:38 -0500

    arreglo

--- resources/views/topics/edit.blade.php
+++ resources/views/topics/edit.blade.php
@@ -13,36 +13,45 @@
                 <div class="row">
                     <div class="col-md-6">
                         <div class="mb-3">
-                            <label for="title" class="form-label">Título del Temas</label>
+                            <label for="title" class="form-label">Título del Tópico</label>
                             <input type="text" class="form-control" name="title" id="title" 
                                    value="{{ old('title', $topic->title) }}" required>
+                            @error('title')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
                         </div>
 
                         <div class="mb-3">
                             <label for="description" class="form-label">Descripción</label>
                             <textarea class="form-control" name="description" id="description" 
-                                      rows="3">{{ old('description', $topic->description) }}</textarea>
+                                      rows="3">{{ old('description', $topic->description ?? '') }}</textarea>
                         </div>
                     </div>
 
                     <div class="col-md-6">
                         <div class="mb-3">
-                            <label for="creation_date" class="form-label">Fecha de creacion </label>
-                            <input type="date" class="form-control" name="create_date" 
-                            value="{{ old('date', \Carbon\Carbon::parse($topic->forum->date ?? now())->format('Y-m-d')) }}" required>
-                            
+                            <label for="creation_date" class="form-label">Fecha de creación</label>
+                            <input type="date" class="form-control" name="creation_date" id="creation_date"
+                            value="{{ old('creation_date', \Carbon\Carbon::parse($topic->creation_date)->format('Y-m-d')) }}" required>
+                            @error('creation_date')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
                         </div>
 
                         <div class="mb-3">
                             <label for="forum_id" class="form-label">Foro</label>
                             <select class="form-select" name="forum_id" id="forum_id" required>
+                                <option value="">Seleccione un foro</option>
                                 @foreach($forums as $forum)
                                     <option value="{{ $forum->id }}" 
-                                        {{ $topic->forum_id == $forum->id ? 'selected' : '' }}>
+                                        {{ old('forum_id', $topic->forum_id) == $forum->id ? 'selected' : '' }}>
                                         {{ $forum->name }}
                                     </option>
                                 @endforeach
                             </select>
+                            @error('forum_id')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                     </div>
                 </div>
